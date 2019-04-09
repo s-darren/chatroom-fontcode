a2d814d0dd40a4bcb1f048595263a2313ad7432d
s-darren <295744570@qq.com>
2019-04-09 15:12:43 +0800

    remake

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import * as React from 'react';
-import './App.css';
-
+import React, { Component } from 'react';
 import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
+import './App.css';
+// import Button from 'antd/lib/button';
+import { Pagination } from 'antd';
+// import 'antd/dist/antd.css';
+// import { Button } from 'antd';
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {/* <Button type="primary">Button</Button> */}
+          <Pagination defaultCurrent={6} total={500} />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
