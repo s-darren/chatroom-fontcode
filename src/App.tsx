a70d04195ae39becb59cd576a84c33228d2f143e
s-darren <295744570@qq.com>
2019-06-04 15:49:14 +0800

    初始化连接websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { List, Avatar, Tooltip, Layout, Button, Input } from 'antd';
+import { List, Avatar, Tooltip, Layout, Button, Input, Modal, InputNumber, message } from 'antd';
+const address = '127.0.0.1:3000'
 const { TextArea } = Input;
 const {
   Header, Footer, Sider, Content,
@@ -26,50 +27,119 @@ const data = [
   },
 ];
 let b:addRoomMessage
-class App extends Component {
-  state = {
-    size: 'large',
-  };
+interface normalObj {
+  [propName: string]: any;
+}
+interface webSocketInstance extends normalObj {
+  websocketInstance: WebSocket
+}
 
+class App extends Component<normalObj, webSocketInstance> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      size: 'large',
+      websocketInstance: new WebSocket(`ws://${address}`),
+      visible: false,
+      roomID: 1,
+      sendMessage: ''
+    };
+  }
+  // state: webSocketInstance = {
+  //   size: 'large',
+  //   websocketInstance: new WebSocket(`ws://${address}`)
+  // };
+  componentDidMount() {
+    this.state.websocketInstance.onmessage = this.onWebSocketMessage
+    this.state.websocketInstance.onopen = this.onWebSocketOpen
+    this.setState({
+      visible: true
+    })
+  }
+  onWebSocketOpen = () => {
+    console.log(this)
+  }
+  onWebSocketMessage = (msg: MessageEvent) => {
+    console.log(msg)
+    // this.state.websocketInstance.send('1199')
+    
+  }
+  roomIDWarning = () => {
+    message.warning('请输入聊天室ID');
+  };
+  handleCancel = () => {
+    this.roomIDWarning()
+  }
+  handleOk = () => {
+    this.state.websocketInstance.send(this.state.roomID)
+    this.setState({
+      visible: false
+    })
+  }
+  onRoomIDChange = (val:number | undefined) => {
+    this.setState({
+      roomID: val
+    })
+  }
+  onMessageChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    let message:string = e.target.value
+    console.log(message, this.state.sendMessage)
+    this.setState({
+      sendMessage: message
+    })
+  }
+  sendMessage = () => {
+    this.state.websocketInstance.send(this.state.sendMessage)
+    this.setState({
+      sendMessage: ''
+    })
+  }
   render() {
     const size = this.state.size as "large" | "small" | "default" | undefined;
     return (
       <div className="App">
-      <Layout>
-        <Header>Header</Header>
         <Layout>
+          <Header>Header</Header>
           <Layout>
-            <Content>
-              <List
-                className="chatroom-content"
-                itemLayout="horizontal"
-                dataSource={data}
-                renderItem={item => (
-                  <List.Item>
-                    <List.Item.Meta
-                      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                      title={<><span className="chatroom-content-username">{item.name}</span><Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}><span>{moment().fromNow()}</span></Tooltip></>}
-                      description={<><div>"Ant Design, a design language for background applications, is refined by Ant UED Team"</div></>}
-                    />
-                  </List.Item>
-                )}
-              />
-            </Content>
-            <Footer>
-              <div className="chatroom-footer">
-                <TextArea rows={2} />
-                <Button type="primary" ghost  size={size} className="chatroom-footer-button">发送</Button>
-              </div>
-            </Footer>
+            <Layout>
+              <Content>
+                <List
+                  className="chatroom-content"
+                  itemLayout="horizontal"
+                  dataSource={data}
+                  renderItem={item => (
+                    <List.Item>
+                      <List.Item.Meta
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                        title={<><span className="chatroom-content-username">{item.name}</span><Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}><span>{moment().fromNow()}</span></Tooltip></>}
+                        description={<><div>"Ant Design, a design language for background applications, is refined by Ant UED Team"</div></>}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </Content>
+              <Footer>
+                <div className="chatroom-footer">
+                  <TextArea rows={2} value={this.state.sendMessage} onChange={this.onMessageChange}/>
+                  <Button type="primary" ghost  size={size} className="chatroom-footer-button" onClick={this.sendMessage}>发送</Button>
+                </div>
+              </Footer>
+            </Layout>
+            <Sider>
+              <Button type="primary"></Button>
+              <Button type="primary"></Button>
+              <Button type="primary"></Button>
+            </Sider>
           </Layout>
-          <Sider>
-            <Button type="primary"></Button>
-            <Button type="primary"></Button>
-            <Button type="primary"></Button>
-          </Sider>
         </Layout>
-      </Layout>
-        
+        <Modal
+          title="请输入需要进入的聊天室ID"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <InputNumber min={1} max={9999} defaultValue={this.state.roomID} onChange={this.onRoomIDChange} />
+        </Modal>
       </div>
     );
   }
